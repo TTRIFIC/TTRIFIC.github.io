8ed3f8ddf2ab9eb39dc29567a48afe0304b9a387
Matthew <beckerman.m@sbcglobal.net>
2022-07-06 00:59:55 -0500

    updated project link and readme

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,22 +7,29 @@ import {
   ProjectsCard,
   ProjectsIcon,
   ProjectsP,
+  ProjectLink,
 } from "./ProjectsElements";
-import Icon1 from "../../assets/images/react-logo.png";
+import Homepage from "../../assets/images/homepage.png";
 
 const Projects = () => {
   return (
     <ProjectsContainer id="projects">
       <ProjectsH1>Projects</ProjectsH1>
       <ProjectsWrapper>
-        <ProjectsCard>
-          <ProjectsIcon src={Icon1} />
-          <ProjectsH2>This Website</ProjectsH2>
-          <ProjectsP>
-            Created using React, React Router, React Scroll, and Styled
-            Components.
-          </ProjectsP>
-        </ProjectsCard>
+        <ProjectLink
+          href="//github.com/TTRIFIC/TTRIFIC.github.io"
+          target="_blank"
+          aria-label="Website"
+        >
+          <ProjectsCard>
+            <ProjectsIcon src={Homepage} />
+            <ProjectsH2>This Website</ProjectsH2>
+            <ProjectsP>
+              Created using React, React Router, React Scroll, and Styled
+              Components.
+            </ProjectsP>
+          </ProjectsCard>
+        </ProjectLink>
       </ProjectsWrapper>
     </ProjectsContainer>
   );
