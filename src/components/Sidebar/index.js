9f2e14980005620c3235ba12d7254426f0a4d118
Matthew <beckerman.m@sbcglobal.net>
2022-08-01 20:24:09 -0500

    updated resume

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../../assets/files/MatthewBeckermanResumeSP2022.pdf";
+import resume from "../../assets/files/MatthewBeckermanResumeSU2022.pdf";
 import {
   SidebarContainer,
   Icon,
